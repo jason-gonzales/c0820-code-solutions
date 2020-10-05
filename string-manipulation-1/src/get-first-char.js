418ade697efbc8355df2e37d60fa44433a882841
Jason Gonzales <jasongonzo25@gmail.com>
2020-10-05 15:22:27 -0700

    completed string-manipulation-1

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+function getFirstChar(string) {
+  const first = string.charAt(0);
+  return first;
+}
---
+++ string-manipulation-1/src/reverse-word.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+function reverseWord(word) {
+  return word.split('').reverse().join('');
+}
