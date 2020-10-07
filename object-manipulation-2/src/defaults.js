abe85951eb2274d9fe4e9954e279cc01f33668ed
Jason Gonzales <jasongonzo25@gmail.com>
2020-10-06 22:06:50 -0700

    completed object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+const defaults = (target, source) => {
+  for (const prop in source) {
+    if (target[prop] === undefined) {
+      target[prop] = source[prop];
+    }
+  }
+};
+
+// run a for in loop to access the property of source object
+// make an if statement saying that if value of target[key] is undefined
+// then value of target object is equal to value of source object
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,20 @@
 /* eslint-disable no-unused-vars */
+
+const omit = (source, keys) => {
+  const newObj = {};
+  const sourceKey = Object.keys(source);
+  for (let i = 0; i < sourceKey.length; i++) {
+    if (!keys.includes(sourceKey[i])) {
+      newObj[sourceKey[i]] = source[sourceKey[i]];
+    }
+  }
+  return newObj;
+};
+
+// create a new empty object
+// access the properties in the source and place
+// in a variable
+// go through the new variable which is an array
+// find out if value keys does not include in the array
+// if it doesnt then the new object keys is same as
+// source keys
